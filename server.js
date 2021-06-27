3827d5ff0def01c667a3143fdda5731e91327828
adan-khan-au13 <adan.khan8@gmail.com>
2021-06-27 10:02:18 +0530

    shopaholic-MERN-app

--- server.js
+++ server.js
@@ -39,6 +39,10 @@ app.use((err, req, res, next) => {
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  const path = require("path");
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.listen(port, () => {
